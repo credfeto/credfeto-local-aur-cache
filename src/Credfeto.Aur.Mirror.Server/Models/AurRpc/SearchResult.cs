51ee47b22c8da685daa70eeddbcaaf639039ef62
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-25 21:49:21 +0100

    Removed comments

--- src/Credfeto.Aur.Mirror.Server/Models/AurRpc/SearchResult.cs
+++ src/Credfeto.Aur.Mirror.Server/Models/AurRpc/SearchResult.cs
@@ -46,15 +46,12 @@ public sealed class SearchResult
         this.Version = version;
     }
 
-    // "Description":"Fast and simple system info written in C, that can be configured at compile time by editing the config.h file",
     [JsonPropertyName("Description")]
     public string Description { get; }
 
-    // "FirstSubmitted":1603171383,
     [JsonPropertyName("FirstSubmitted")]
     public long FirstSubmitted { get; }
 
-    // "ID":849486
     [JsonPropertyName("ID")]
     public int Id { get; }
 
@@ -64,39 +61,30 @@ public sealed class SearchResult
     [JsonPropertyName("License")]
     public IReadOnlyList<string>? License { get; }
 
-    // "LastModified":1610998028
     [JsonPropertyName("LastModified")]
     public long LastModified { get; }
 
-    // "Maintainer":"McFranko"
     [JsonPropertyName("Maintainer")]
     public string Maintainer { get; }
 
-    // "Name":"afetch-git"
     [JsonPropertyName("Name")]
     public string Name { get; }
 
-    // "NumVotes":3
     [JsonPropertyName("NumVotes")]
     public int NumVotes { get; }
 
-    // "OutOfDate":1687880376
     [JsonPropertyName("OutOfDate")]
     public long? OutOfDate { get; }
 
-    // "PackageBase":"afetch-git"
     [JsonPropertyName("PackageBase")]
     public string PackageBase { get; }
 
-    // "PackageBaseID":158933
     [JsonPropertyName("PackageBaseID")]
     public int PackageBaseId { get; }
 
-    // "Popularity":0
     [JsonPropertyName("Popularity")]
     public double Popularity { get; }
 
-    // "URL":"https://github.com/13-CF/afetch"
     [SuppressMessage(
         category: "Microsoft.Naming",
         checkId: "CA1056: Uri properties should not be strings",
@@ -105,7 +93,6 @@ public sealed class SearchResult
     [JsonPropertyName("URL")]
     public string Url { get; }
 
-    // "URLPath":"/cgit/aur.git/snapshot/afetch-git.tar.gz"
     [SuppressMessage(
         category: "Microsoft.Naming",
         checkId: "CA1056: Uri properties should not be strings",
@@ -114,7 +101,6 @@ public sealed class SearchResult
     [JsonPropertyName("URLPath")]
     public string UrlPath { get; }
 
-    // "Version":"1-1"
     [JsonPropertyName("Version")]
     public string Version { get; }
 }
