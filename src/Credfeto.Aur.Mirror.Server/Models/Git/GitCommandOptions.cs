e50a65a64294f16a3ef073a4c0f4fb5cd8221b83
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-09-30 12:00:10 +0000

    Cleanup: GitCommandOptions.cs

--- src/Credfeto.Aur.Mirror.Server/Models/Git/GitCommandOptions.cs
+++ src/Credfeto.Aur.Mirror.Server/Models/Git/GitCommandOptions.cs
@@ -5,12 +5,15 @@ using System.Text;
 namespace Credfeto.Aur.Mirror.Server.Models.Git;
 
 [DebuggerDisplay("Repo:{RepositoryName}, Service: {Service} Refs: {AdvertiseRefs} {EndStreamWithNull}")]
-public readonly record struct GitCommandOptions(string RepositoryName, string Service, bool AdvertiseRefs, bool EndStreamWithNull)
+public readonly record struct GitCommandOptions(
+    string RepositoryName,
+    string Service,
+    bool AdvertiseRefs,
+    bool EndStreamWithNull
+)
 {
     public string ContentType =>
-        this.AdvertiseRefs
-            ? $"application/x-{this.Service}-advertisement"
-            : $"application/x-{this.Service}";
+        this.AdvertiseRefs ? $"application/x-{this.Service}-advertisement" : $"application/x-{this.Service}";
 
     public string BuildCommand(string repositoryBasePath)
     {
@@ -19,15 +22,15 @@ public readonly record struct GitCommandOptions(string RepositoryName, string Se
             throw new InvalidOperationException();
         }
 
-        StringBuilder builder = new StringBuilder().Append(value: this.Service, startIndex: 4, this.Service.Length - 4)
-                                                   .Append(" --stateless-rpc");
+        StringBuilder builder = new StringBuilder()
+            .Append(value: this.Service, startIndex: 4, this.Service.Length - 4)
+            .Append(" --stateless-rpc");
 
         if (this.AdvertiseRefs)
         {
             builder = builder.Append(" --advertise-refs");
         }
 
-        return builder.Append($@" ""{repositoryBasePath}""")
-                      .ToString();
+        return builder.Append($@" ""{repositoryBasePath}""").ToString();
     }
 }
