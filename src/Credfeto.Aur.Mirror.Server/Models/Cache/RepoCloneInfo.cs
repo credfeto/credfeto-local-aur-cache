1d65c8da9dfa9aada08b4d4e615d4a85a552a56f
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-04 13:43:02 +0000

    Cleanup: RepoCloneInfo.cs

--- src/Credfeto.Aur.Mirror.Server/Models/Cache/RepoCloneInfo.cs
+++ src/Credfeto.Aur.Mirror.Server/Models/Cache/RepoCloneInfo.cs
@@ -8,7 +8,11 @@ namespace Credfeto.Aur.Mirror.Server.Models.Cache;
 [DebuggerDisplay("Repo: {Repo} Last Cloned: {LastCloned}")]
 public sealed class RepoCloneInfo
 {
-    [SuppressMessage(category: "Roslynator.Analyzers", checkId: "RCS1231:Make ref read only", Justification = "Not for json serialization")]
+    [SuppressMessage(
+        category: "Roslynator.Analyzers",
+        checkId: "RCS1231:Make ref read only",
+        Justification = "Not for json serialization"
+    )]
     [JsonConstructor]
     public RepoCloneInfo(string repo, DateTimeOffset lastCloned)
     {
