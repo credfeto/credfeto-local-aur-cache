81f3d34f4a3fa7c8bd570a8eba3801bffd8d6aeb
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-04 13:31:09 +0000

    Cleanup: Endpoints.Cache.cs

--- src/Credfeto.Aur.Mirror.Server/Helpers/Endpoints.Cache.cs
+++ src/Credfeto.Aur.Mirror.Server/Helpers/Endpoints.Cache.cs
@@ -14,13 +14,15 @@ internal static partial class Endpoints
     {
         Console.WriteLine("Configuring Cache Endpoint");
 
-        app.MapGet(pattern: "/cache",
-                   handler: async static (ILocallyInstalled locallyInstalled, CancellationToken cancellationToken) =>
-                            {
-                                IReadOnlyList<RepoCloneInfo> cached = await locallyInstalled.GetRecentlyClonedAsync(cancellationToken);
+        app.MapGet(
+            pattern: "/cache",
+            handler: static async (ILocallyInstalled locallyInstalled, CancellationToken cancellationToken) =>
+            {
+                IReadOnlyList<RepoCloneInfo> cached = await locallyInstalled.GetRecentlyClonedAsync(cancellationToken);
 
-                                return Results.Ok(cached);
-                            });
+                return Results.Ok(cached);
+            }
+        );
 
         return app;
     }
