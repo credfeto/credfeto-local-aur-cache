08264c30afe8a8ae4c35ffbe7f617440a51168c8
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-30 07:29:42 +0000

    Cleanup: GitCommandLine.cs

--- src/Credfeto.Aur.Mirror.Server/Helpers/GitCommandLine.cs
+++ src/Credfeto.Aur.Mirror.Server/Helpers/GitCommandLine.cs
@@ -20,16 +20,16 @@ internal static class GitCommandLine
         EnsureNotLocked(repoUrl: clonePath, workingDirectory: repoPath);
 
         ProcessStartInfo psi = new()
-                               {
-                                   FileName = gitExecutable,
-                                   WorkingDirectory = repoPath,
-                                   Arguments = arguments,
-                                   RedirectStandardOutput = true,
-                                   RedirectStandardError = true,
-                                   UseShellExecute = false,
-                                   CreateNoWindow = true,
-                                   Environment = { ["GIT_REDIRECT_STDERR"] = "2>&1" },
-                               };
+        {
+            FileName = gitExecutable,
+            WorkingDirectory = repoPath,
+            Arguments = arguments,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            Environment = { ["GIT_REDIRECT_STDERR"] = "2>&1" },
+        };
 
         using (Process? process = Process.Start(psi))
         {
