f2c50a4b16d14a1a2f7d4cb78cd6e0c7008a1f5f
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-30 07:33:16 +0000

    Cleanup: StartupBanner.cs

--- src/Credfeto.Aur.Mirror.Server/Helpers/StartupBanner.cs
+++ src/Credfeto.Aur.Mirror.Server/Helpers/StartupBanner.cs
@@ -5,7 +5,7 @@ namespace Credfeto.Aur.Mirror.Server.Helpers;
 
 // https://www.figlet.org/examples.html
 [GenerateFiggleText("Banner", "basic", "AUR Cache")]
-internal static  partial class StartupBanner
+internal static partial class StartupBanner
 {
     public static void Show()
     {
