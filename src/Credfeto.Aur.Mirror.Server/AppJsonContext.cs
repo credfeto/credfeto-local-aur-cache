ee38ff0565030f9243a159860a3a7f2075351ddb
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-17 22:21:50 +0000

    Corrected namespace

--- src/Credfeto.Aur.Mirror.Server/AppJsonContext.cs
+++ src/Credfeto.Aur.Mirror.Server/AppJsonContext.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Credfeto.Aur.Mirror.Models;
 using Credfeto.Aur.Mirror.Models.AurRpc;
 using Credfeto.Aur.Mirror.Models.Cache;
 
-namespace Credfeto.Aur.Mirror.Models;
+namespace Credfeto.Aur.Mirror.Server;
 
 [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Serialization | JsonSourceGenerationMode.Metadata,
                              PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
