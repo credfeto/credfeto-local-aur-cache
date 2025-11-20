10b75374b8a7187b452c9bba0f41ea29bebf26d0
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-20 14:41:20 +0000

    Cleanup: AppJsonContext.cs

--- src/Credfeto.Aur.Mirror.Server/AppJsonContext.cs
+++ src/Credfeto.Aur.Mirror.Server/AppJsonContext.cs
@@ -6,11 +6,13 @@ using Credfeto.Aur.Mirror.Models.Cache;
 
 namespace Credfeto.Aur.Mirror.Server;
 
-[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Serialization | JsonSourceGenerationMode.Metadata,
-                             PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-                             DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-                             WriteIndented = false,
-                             IncludeFields = false)]
+[JsonSourceGenerationOptions(
+    GenerationMode = JsonSourceGenerationMode.Serialization | JsonSourceGenerationMode.Metadata,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    WriteIndented = false,
+    IncludeFields = false
+)]
 [JsonSerializable(typeof(PongDto))]
 [JsonSerializable(typeof(RpcResponse))]
 [JsonSerializable(typeof(SearchResult))]
