013fb10a29641c7038f3fe8c83e1fe96eeedc2c0
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-24 08:35:17 +0000

    Cleanup: GitRepositoryLockedException.cs

--- src/Credfeto.Aur.Mirror.Git/Exceptions/GitRepositoryLockedException.cs
+++ src/Credfeto.Aur.Mirror.Git/Exceptions/GitRepositoryLockedException.cs
@@ -4,17 +4,11 @@ namespace Credfeto.Aur.Mirror.Git.Exceptions;
 
 public sealed class GitRepositoryLockedException : Exception
 {
-    public GitRepositoryLockedException()
-    {
-    }
+    public GitRepositoryLockedException() { }
 
     public GitRepositoryLockedException(string? message)
-        : base(message)
-    {
-    }
+        : base(message) { }
 
     public GitRepositoryLockedException(string? message, Exception? innerException)
-        : base(message: message, innerException: innerException)
-    {
-    }
+        : base(message: message, innerException: innerException) { }
 }
