bff05c4b60c16b2a5f8d523343074aec8f321abe
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-24 11:52:38 +0000

    Cleanup: LocallyInstalledLoggingExtensions.cs

--- src/Credfeto.Aur.Mirror.Git/Services/LoggingExtensions/LocallyInstalledLoggingExtensions.cs
+++ src/Credfeto.Aur.Mirror.Git/Services/LoggingExtensions/LocallyInstalledLoggingExtensions.cs
@@ -6,8 +6,16 @@ namespace Credfeto.Aur.Mirror.Git.Services.LoggingExtensions;
 public static partial class LocallyInstalledLoggingExtensions
 {
     [LoggerMessage(LogLevel.Information, EventId = 1, Message = "Adding {repo} to clone cache for {timestamp}")]
-    public static partial void AddingToCloneCache(this ILogger<LocallyInstalled> logger, string repo, DateTimeOffset timestamp);
+    public static partial void AddingToCloneCache(
+        this ILogger<LocallyInstalled> logger,
+        string repo,
+        DateTimeOffset timestamp
+    );
 
     [LoggerMessage(LogLevel.Information, EventId = 2, Message = "Updating {repo} in clone cache to {timestamp}")]
-    public static partial void UpdatingCloneCache(this ILogger<LocallyInstalled> logger, string repo, DateTimeOffset timestamp);
+    public static partial void UpdatingCloneCache(
+        this ILogger<LocallyInstalled> logger,
+        string repo,
+        DateTimeOffset timestamp
+    );
 }
