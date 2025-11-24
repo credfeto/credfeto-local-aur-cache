e4fec66b877265b4b22b934befd6848ac2fbbd33
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-24 11:59:57 +0000

    Cleanup: AurRpcLoggingExtensions.cs

--- src/Credfeto.Aur.Mirror.Rpc/Services/LoggingExtensions/AurRpcLoggingExtensions.cs
+++ src/Credfeto.Aur.Mirror.Rpc/Services/LoggingExtensions/AurRpcLoggingExtensions.cs
@@ -17,25 +17,73 @@ internal static partial class AurRpcLoggingExtensions
         logger.PackageInfo(string.Join(separator: ", ", values: packages));
     }
 
-    [LoggerMessage(LogLevel.Warning, EventId = 3, Message = "Failed to retrieve package infos: {packages} => {message}")]
-    private static partial void FailedToGetUpstreamPackageInfo(this ILogger<AurRpc> logger, string packages, string message, Exception exception);
+    [LoggerMessage(
+        LogLevel.Warning,
+        EventId = 3,
+        Message = "Failed to retrieve package infos: {packages} => {message}"
+    )]
+    private static partial void FailedToGetUpstreamPackageInfo(
+        this ILogger<AurRpc> logger,
+        string packages,
+        string message,
+        Exception exception
+    );
 
-    public static void FailedToGetUpstreamPackageInfo(this ILogger<AurRpc> logger, IReadOnlyList<string> packages, string message, Exception exception)
+    public static void FailedToGetUpstreamPackageInfo(
+        this ILogger<AurRpc> logger,
+        IReadOnlyList<string> packages,
+        string message,
+        Exception exception
+    )
     {
-        logger.FailedToGetUpstreamPackageInfo(string.Join(separator: ", ", values: packages), message: message, exception: exception);
+        logger.FailedToGetUpstreamPackageInfo(
+            string.Join(separator: ", ", values: packages),
+            message: message,
+            exception: exception
+        );
     }
 
-    [LoggerMessage(LogLevel.Warning, EventId = 4, Message = "Failed Upstream search for: {keyword} by {by} => {message}")]
-    public static partial void FailedToSearchUpstreamPackageInfo(this ILogger<AurRpc> logger, string keyword, string by, string message, Exception exception);
+    [LoggerMessage(
+        LogLevel.Warning,
+        EventId = 4,
+        Message = "Failed Upstream search for: {keyword} by {by} => {message}"
+    )]
+    public static partial void FailedToSearchUpstreamPackageInfo(
+        this ILogger<AurRpc> logger,
+        string keyword,
+        string by,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Information, EventId = 5, Message = "Package {package} is not cached locally")]
     public static partial void NotCachedLocally(this ILogger<AurRpc> logger, string package);
 
-    [LoggerMessage(LogLevel.Information, EventId = 6, Message = "Package {package} was not accessed recently (Accessed {lastAccessed} was {age} ago Max Age {maxAge})")]
-    public static partial void NotAccessedRecently(this ILogger<AurRpc> logger, string package, DateTimeOffset lastAccessed, TimeSpan age, TimeSpan maxAge);
+    [LoggerMessage(
+        LogLevel.Information,
+        EventId = 6,
+        Message = "Package {package} was not accessed recently (Accessed {lastAccessed} was {age} ago Max Age {maxAge})"
+    )]
+    public static partial void NotAccessedRecently(
+        this ILogger<AurRpc> logger,
+        string package,
+        DateTimeOffset lastAccessed,
+        TimeSpan age,
+        TimeSpan maxAge
+    );
 
-    [LoggerMessage(LogLevel.Information, EventId = 7, Message = "Package {package} was not requested recently (Requested {lastRequested} was {age} ago Max Age {maxAge})")]
-    public static partial void NotRequestedRecently(this ILogger<AurRpc> logger, string package, DateTimeOffset lastRequested, TimeSpan age, TimeSpan maxAge);
+    [LoggerMessage(
+        LogLevel.Information,
+        EventId = 7,
+        Message = "Package {package} was not requested recently (Requested {lastRequested} was {age} ago Max Age {maxAge})"
+    )]
+    public static partial void NotRequestedRecently(
+        this ILogger<AurRpc> logger,
+        string package,
+        DateTimeOffset lastRequested,
+        TimeSpan age,
+        TimeSpan maxAge
+    );
 
     [LoggerMessage(LogLevel.Information, EventId = 8, Message = "Using local cache for package infos: {packages}")]
     private static partial void UsingLocalCache(this ILogger<AurRpc> logger, string packages);
