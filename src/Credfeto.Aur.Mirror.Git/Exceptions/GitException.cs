f54dd19a409fea824b0346556ef5e007821fdcd0
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-24 08:28:27 +0000

    Cleanup: GitException.cs

--- src/Credfeto.Aur.Mirror.Git/Exceptions/GitException.cs
+++ src/Credfeto.Aur.Mirror.Git/Exceptions/GitException.cs
@@ -1,20 +1,14 @@
-﻿using System;
+using System;
 
 namespace Credfeto.Aur.Mirror.Git.Exceptions;
 
 public sealed class GitException : Exception
 {
-    public GitException()
-    {
-    }
+    public GitException() { }
 
     public GitException(string? message)
-        : base(message)
-    {
-    }
+        : base(message) { }
 
     public GitException(string? message, Exception? innerException)
-        : base(message: message, innerException: innerException)
-    {
-    }
+        : base(message: message, innerException: innerException) { }
 }
