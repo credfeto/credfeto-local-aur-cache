49cfbdff2f2862b4d5020241d537c2b1ea02cbe1
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-12-18 00:48:08 +0000

    Cleanup: SearchResultMock.cs

--- src/Credfeto.Aur.Mirror.Cache.Tests/Mocks/SearchResultMock.cs
+++ src/Credfeto.Aur.Mirror.Cache.Tests/Mocks/SearchResultMock.cs
@@ -9,30 +9,34 @@ internal sealed class SearchResultMock : MockBase<SearchResult>
     private long _lastModified;
 
     public SearchResultMock()
-        : base(new(description: "",
-                   firstSubmitted: 123456789,
-                   id: 44,
-                   keywords: null,
-                   license: null,
-                   depends: null,
-                   makeDepends: null,
-                   optDepends: null,
-                   checkDepends: null,
-                   conflicts: null,
-                   replaces: null,
-                   groups: null,
-                   coMaintainers: null,
-                   lastModified: 1234567890,
-                   maintainer: "example",
-                   name: "example-package",
-                   numVotes: 42,
-                   outOfDate: null,
-                   packageBase: "example-base",
-                   packageBaseId: 41,
-                   popularity: 184,
-                   url: "https://example.com",
-                   urlPath: "/package",
-                   version: "1.23"))
+        : base(
+            new(
+                description: "",
+                firstSubmitted: 123456789,
+                id: 44,
+                keywords: null,
+                license: null,
+                depends: null,
+                makeDepends: null,
+                optDepends: null,
+                checkDepends: null,
+                conflicts: null,
+                replaces: null,
+                groups: null,
+                coMaintainers: null,
+                lastModified: 1234567890,
+                maintainer: "example",
+                name: "example-package",
+                numVotes: 42,
+                outOfDate: null,
+                packageBase: "example-base",
+                packageBaseId: 41,
+                popularity: 184,
+                url: "https://example.com",
+                urlPath: "/package",
+                version: "1.23"
+            )
+        )
     {
         SearchResult current = this;
         this._lastModified = current.LastModified;
@@ -42,29 +46,31 @@ internal sealed class SearchResultMock : MockBase<SearchResult>
     {
         long lastModified = Interlocked.Increment(ref this._lastModified);
 
-        return new(description: "",
-                   firstSubmitted: 123456789,
-                   id: 44,
-                   keywords: null,
-                   license: null,
-                   depends: null,
-                   makeDepends: null,
-                   optDepends: null,
-                   checkDepends: null,
-                   conflicts: null,
-                   replaces: null,
-                   groups: null,
-                   coMaintainers: null,
-                   lastModified: lastModified,
-                   maintainer: "example",
-                   name: "example-package",
-                   numVotes: 42,
-                   outOfDate: null,
-                   packageBase: "example-base",
-                   packageBaseId: 41,
-                   popularity: 184,
-                   url: "https://example.com",
-                   urlPath: "/package",
-                   version: "1.23");
+        return new(
+            description: "",
+            firstSubmitted: 123456789,
+            id: 44,
+            keywords: null,
+            license: null,
+            depends: null,
+            makeDepends: null,
+            optDepends: null,
+            checkDepends: null,
+            conflicts: null,
+            replaces: null,
+            groups: null,
+            coMaintainers: null,
+            lastModified: lastModified,
+            maintainer: "example",
+            name: "example-package",
+            numVotes: 42,
+            outOfDate: null,
+            packageBase: "example-base",
+            packageBaseId: 41,
+            popularity: 184,
+            url: "https://example.com",
+            urlPath: "/package",
+            version: "1.23"
+        );
     }
 }
