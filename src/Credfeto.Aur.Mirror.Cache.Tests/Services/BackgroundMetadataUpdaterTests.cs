3519aa978c95ceb53a5920fed7bbd849cb5b38c3
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-12-18 00:58:40 +0000

    Cleanup: BackgroundMetadataUpdaterTests.cs

--- src/Credfeto.Aur.Mirror.Cache.Tests/Services/BackgroundMetadataUpdaterTests.cs
+++ src/Credfeto.Aur.Mirror.Cache.Tests/Services/BackgroundMetadataUpdaterTests.cs
@@ -30,17 +30,20 @@ public sealed class BackgroundMetadataUpdaterTests : TestBase
     [Fact]
     public async Task RequestUpdateAsync_ValidPackageName_UpdateCalled()
     {
-        _ = this._localAurMetadata.Get("example-package")
-                .Returns(this._examplePackage);
+        _ = this._localAurMetadata.Get("example-package").Returns(this._examplePackage);
 
         const string packageName = "example-package";
 
         await this._updater.RequestUpdateAsync(packageName: packageName, update: Update, this.CancellationToken());
 
-        _ = this._localAurMetadata.Received(1)
-                .Get("example-package");
-        await this._localAurMetadata.Received(1)
-                  .UpdateAsync(Arg.Is<Package>(p => StringComparer.Ordinal.Equals(x: p.PackageName, y: "example-package")), Arg.Any<Action<Package>>(), Arg.Any<CancellationToken>());
+        _ = this._localAurMetadata.Received(1).Get("example-package");
+        await this
+            ._localAurMetadata.Received(1)
+            .UpdateAsync(
+                Arg.Is<Package>(p => StringComparer.Ordinal.Equals(x: p.PackageName, y: "example-package")),
+                Arg.Any<Action<Package>>(),
+                Arg.Any<CancellationToken>()
+            );
 
         return;
 
@@ -53,17 +56,16 @@ public sealed class BackgroundMetadataUpdaterTests : TestBase
     [Fact]
     public async Task RequestUpdateAsync_InvalidPackageName_WritesToQueue()
     {
-        _ = this._localAurMetadata.Get("invalid-package")
-                .Returns(this._packageDoesNotExist);
+        _ = this._localAurMetadata.Get("invalid-package").Returns(this._packageDoesNotExist);
 
         const string packageName = "invalid-package";
 
         await this._updater.RequestUpdateAsync(packageName: packageName, update: Update, this.CancellationToken());
 
-        await this._localAurMetadata.DidNotReceive()
-                  .UpdateAsync(Arg.Any<Package>(), Arg.Any<Action<Package>>(), Arg.Any<CancellationToken>());
-        _ = this._localAurMetadata.Received(1)
-                .Get("invalid-package");
+        await this
+            ._localAurMetadata.DidNotReceive()
+            .UpdateAsync(Arg.Any<Package>(), Arg.Any<Action<Package>>(), Arg.Any<CancellationToken>());
+        _ = this._localAurMetadata.Received(1).Get("invalid-package");
 
         return;
 
