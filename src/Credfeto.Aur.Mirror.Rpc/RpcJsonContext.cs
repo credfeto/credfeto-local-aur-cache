03494afd2945edd0276b780e9e620a7fe81c4cea
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-12-18 00:14:29 +0000

    Cleanup: RpcJsonContext.cs

--- src/Credfeto.Aur.Mirror.Rpc/RpcJsonContext.cs
+++ src/Credfeto.Aur.Mirror.Rpc/RpcJsonContext.cs
@@ -3,11 +3,13 @@ using Credfeto.Aur.Mirror.Models.AurRpc;
 
 namespace Credfeto.Aur.Mirror.Rpc;
 
-[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Serialization | JsonSourceGenerationMode.Metadata,
-                             PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-                             DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-                             WriteIndented = false,
-                             IncludeFields = false)]
+[JsonSourceGenerationOptions(
+    GenerationMode = JsonSourceGenerationMode.Serialization | JsonSourceGenerationMode.Metadata,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    WriteIndented = false,
+    IncludeFields = false
+)]
 [JsonSerializable(typeof(SearchResult))]
 [JsonSerializable(typeof(RpcResponse))]
 internal sealed partial class RpcJsonContext : JsonSerializerContext;
